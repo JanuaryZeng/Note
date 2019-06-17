db5bbe9cf2a74af4d3634da3d0d3352189fcde7d
JanuaryZeng <15841331413@163.com>
2019-06-17 16:55:11 +0800

    first

---
+++ app/src/main/java/cn/edu/nuc/note/activity/HomeActivity.java
@@ -0,0 +1,137 @@
+package cn.edu.nuc.note.activity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import cn.edu.nuc.note.R;
+import cn.edu.nuc.note.view.fragment.HomeFragment;
+import cn.edu.nuc.note.view.fragment.MessageFragment;
+import cn.edu.nuc.note.view.fragment.MineFragment;
+
+public class HomeActivity extends BaseActivity implements View.OnClickListener {
+
+    private FragmentManager fm;
+    private HomeFragment mHomeFragment;
+    private Fragment mCommonFragmentOne;
+    private MessageFragment mMessageFragment;
+    private MineFragment mMineFragment;
+    private Fragment mCurrent;
+
+    private RelativeLayout mHomeLayout;
+    private RelativeLayout mPondLayout;
+    private RelativeLayout mMessageLayout;
+    private RelativeLayout mMineLayout;
+    private TextView mHomeView;
+    private TextView mPondView;
+    private TextView mMessageView;
+    private TextView mMineView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        //实例化我们页面中所有的控件
+        initView();
+        //添加默认要显示的Fragment
+        mHomeFragment = new HomeFragment();//新建Home的activity
+        fm = getSupportFragmentManager();   //获取管理fragment的管理对象
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();//开始事务处理
+        fragmentTransaction.replace(R.id.content_layout, mHomeFragment);//等同与先remove在add,切换fragment
+        fragmentTransaction.commit();//执行切换的动作
+    }
+    private void initView() {
+        mHomeLayout = findViewById(R.id.home_layout_view);
+        mHomeLayout.setOnClickListener(this);
+        mPondLayout = findViewById(R.id.pond_layout_view);
+        mPondLayout.setOnClickListener(this);
+        mMessageLayout =  findViewById(R.id.message_layout_view);
+        mMessageLayout.setOnClickListener(this);
+        mMineLayout = findViewById(R.id.mine_layout_view);
+        mMineLayout.setOnClickListener(this);
+
+        mHomeView = (TextView) findViewById(R.id.home_image_view);
+        mPondView = (TextView) findViewById(R.id.fish_image_view);
+        mMessageView = (TextView) findViewById(R.id.message_image_view);
+        mMineView = (TextView) findViewById(R.id.mine_image_view);
+        mHomeView.setBackgroundResource(R.drawable.comui_tab_home_selected);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void hideFragment(Fragment fragment, FragmentTransaction ft) {
+        if (fragment != null) {
+            ft.hide(fragment);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        //处理切换Fragment逻辑
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        switch (v.getId()) {
+            case R.id.home_layout_view:
+                //设置点击UI会变
+                mHomeView.setBackgroundResource(R.drawable.comui_tab_home_selected);
+                mPondView.setBackgroundResource(R.drawable.comui_tab_pond);
+                mMessageView.setBackgroundResource(R.drawable.comui_tab_message);
+                mMineView.setBackgroundResource(R.drawable.comui_tab_person);
+
+                //隐藏其他fragment
+                hideFragment(mCommonFragmentOne, fragmentTransaction);
+                hideFragment(mMessageFragment, fragmentTransaction);
+                hideFragment(mMineFragment, fragmentTransaction);
+                //将我们的HomeFragment显示出来,不为空则显示.为空创建显示
+                if (mHomeFragment == null) {
+                    mHomeFragment = new HomeFragment();
+                    fragmentTransaction.add(R.id.content_layout, mHomeFragment);
+                } else {
+                    mCurrent = mHomeFragment;
+                    fragmentTransaction.show(mHomeFragment);
+                }
+                break;
+            case R.id.message_layout_view:
+                mMessageView.setBackgroundResource(R.drawable.comui_tab_message_selected);
+                mHomeView.setBackgroundResource(R.drawable.comui_tab_home);
+                mPondView.setBackgroundResource(R.drawable.comui_tab_pond);
+                mMineView.setBackgroundResource(R.drawable.comui_tab_person);
+
+                hideFragment(mCommonFragmentOne, fragmentTransaction);
+                hideFragment(mHomeFragment, fragmentTransaction);
+                hideFragment(mMineFragment, fragmentTransaction);
+                if (mMessageFragment == null) {
+                    mMessageFragment = new MessageFragment();
+                    fragmentTransaction.add(R.id.content_layout, mMessageFragment);
+                } else {
+                    mCurrent = mMessageFragment;
+                    fragmentTransaction.show(mMessageFragment);
+                }
+                break;
+            case R.id.mine_layout_view:
+                mMineView.setBackgroundResource(R.drawable.comui_tab_person_selected);
+                mHomeView.setBackgroundResource(R.drawable.comui_tab_home);
+                mPondView.setBackgroundResource(R.drawable.comui_tab_pond);
+                mMessageView.setBackgroundResource(R.drawable.comui_tab_message);
+                hideFragment(mCommonFragmentOne, fragmentTransaction);
+                hideFragment(mMessageFragment, fragmentTransaction);
+                hideFragment(mHomeFragment, fragmentTransaction);
+                if (mMineFragment == null) {
+                    mMineFragment = new MineFragment();
+                    fragmentTransaction.add(R.id.content_layout, mMineFragment);
+                } else {
+                    mCurrent = mMineFragment;
+                    fragmentTransaction.show(mMineFragment);
+                }
+                break;
+        }
+
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/cn/edu/nuc/note/view/fragment/BaseFragment.java
@@ -0,0 +1,35 @@
+package cn.edu.nuc.note.view.fragment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import cn.edu.nuc.note.R;
+
+
+public class BaseFragment extends Fragment {
+    protected Activity mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
